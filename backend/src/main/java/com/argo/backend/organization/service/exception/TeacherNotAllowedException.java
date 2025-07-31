55b71e7bab713fadd8f87e1d954568bcba7fce97
민사빈 <binine2@naver.com>
2025-07-31 17:47:03 +0900

    remove: organization 기능 디렉토리 삭제

--- backend/src/main/java/com/argo/backend/organization/service/exception/TeacherNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.argo.backend.organization.service.exception;
-
-public class TeacherNotAllowedException extends RuntimeException {
-    public TeacherNotAllowedException(String message) {
-        super(message);
-    }
-}
