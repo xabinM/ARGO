88cc6e3bff260a3526e8adadb77d49b40aafecd7
민사빈 <binine2@naver.com>
2025-08-07 14:40:24 +0900

    refactor: gps api 응답 시 success 필드 추가

--- backend/src/main/java/com/argo/backend/gps/dto/UserCoordinatesDto.java
+++ backend/src/main/java/com/argo/backend/gps/dto/UserCoordinatesDto.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 @Getter
 @AllArgsConstructor
-public class UserCoordinatesResponse {
+public class UserCoordinatesDto {
 
     private Long userId;
     private BigDecimal latitude;
