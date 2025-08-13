0125888e525044e65c716b41079e1b5a3fa3e9af
민사빈 <binine2@naver.com>
2025-08-13 13:50:20 +0900

    fix: 미션 생성 테스트 코드에서 spot mock 객체 생성을 실제 객체로 변경 및 클린 코드

--- backend/src/test/java/com/argo/backend/mission/service/MissionServiceTest.java
+++ backend/src/test/java/com/argo/backend/mission/service/MissionServiceTest.java
@@ -34,19 +34,24 @@ import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MissionServiceTest {
 
-    @Mock private MissionSessionRepository missionSessionRepository;
-    @Mock private SpotRepository spotRepository;
-    @Mock private TeamRepository teamRepository;
-    @Mock private CardRepository cardRepository;
-    @Mock private TeamCardRepository teamCardRepository;
-    @Mock private SelfieProblemRepository selfieProblemRepository;
+    @Mock
+    private MissionSessionRepository missionSessionRepository;
+    @Mock
+    private SpotRepository spotRepository;
+    @Mock
+    private TeamRepository teamRepository;
+    @Mock
+    private CardRepository cardRepository;
+    @Mock
+    private TeamCardRepository teamCardRepository;
+    @Mock
+    private SelfieProblemRepository selfieProblemRepository;
 
     @InjectMocks
     private MissionService missionService;
@@ -55,14 +60,14 @@ class MissionServiceTest {
 
     @Test
     void createMission_미션세션없고_문제존재시_성공() {
-        Long teamId = 1L;
-        Long spotId = 2L;
+        final Long teamId = 1L;
+        final Long spotId = 2L;
 
-        Spot spot = mock(Spot.class);
-        Problem problem = mock(QuizProblem.class);
+        final Spot spot = new Spot();
+        final Problem problem = mock(QuizProblem.class);
         spot.setProblems(List.of(problem));
 
-        Team team = mock(Team.class);
+        final Team team = mock(Team.class);
 
         when(spotRepository.findById(spotId)).thenReturn(Optional.of(spot));
         when(teamRepository.findById(teamId)).thenReturn(Optional.of(team));
@@ -78,12 +83,12 @@ class MissionServiceTest {
 
     @Test
     void createMission_문제없으면_예외발생() {
-        Long teamId = 1L;
-        Long spotId = 2L;
+        final Long teamId = 1L;
+        final Long spotId = 2L;
 
-        Spot spot = mock(Spot.class);
+        final Spot spot = mock(Spot.class);
         spot.setProblems(Collections.emptyList());
-        Team team = mock(Team.class);
+        final Team team = mock(Team.class);
 
         when(spotRepository.findById(spotId)).thenReturn(Optional.of(spot));
         when(teamRepository.findById(teamId)).thenReturn(Optional.of(team));
@@ -96,13 +101,13 @@ class MissionServiceTest {
 
     @Test
     void createMission_이미완료된미션이면_예외발생() {
-        Long teamId = 1L;
-        Long spotId = 2L;
+        final Long teamId = 1L;
+        final Long spotId = 2L;
 
-        Spot spot = mock(Spot.class);
-        Team team = mock(Team.class);
+        final Spot spot = mock(Spot.class);
+        final Team team = mock(Team.class);
 
-        MissionSession completedSession = mock(MissionSession.class);
+        final MissionSession completedSession = mock(MissionSession.class);
         when(completedSession.getStatus()).thenReturn(MissionSessionStatus.COMPLETED);
 
         when(spotRepository.findById(spotId)).thenReturn(Optional.of(spot));
@@ -117,8 +122,8 @@ class MissionServiceTest {
 
     @Test
     void submitMission_시작안된미션이면_예외발생() {
-        Long missionId = 1L;
-        MissionSession missionSession = mock(MissionSession.class);
+        final Long missionId = 1L;
+        final MissionSession missionSession = mock(MissionSession.class);
 
         when(missionSessionRepository.findById(missionId)).thenReturn(Optional.of(missionSession));
         when(missionSession.isStatusStarted()).thenReturn(false);
@@ -129,18 +134,18 @@ class MissionServiceTest {
 
     @Test
     void submitMission_성공케이스() {
-        Long missionId = 1L;
-        MissionSession missionSession = mock(MissionSession.class);
-        Spot spot = mock(Spot.class);
+        final Long missionId = 1L;
+        final MissionSession missionSession = mock(MissionSession.class);
+        final Spot spot = mock(Spot.class);
         spot.setId(10L);
-        Card card = mock(Card.class);
+        final Card card = mock(Card.class);
 
         when(missionSessionRepository.findById(missionId)).thenReturn(Optional.of(missionSession));
         when(missionSession.isStatusStarted()).thenReturn(true);
         when(missionSession.getSpot()).thenReturn(spot);
         when(cardRepository.findAllBySpotId(spot.getId())).thenReturn(List.of(card));
 
-        MissionSubmitDto dto = missionService.submitMission(missionId, true);
+        final MissionSubmitDto dto = missionService.submitMission(missionId, true);
 
         assertThat(dto.successful()).isTrue();
         verify(teamCardRepository, times(1)).save(any(TeamCard.class));
@@ -148,11 +153,11 @@ class MissionServiceTest {
 
     @Test
     void submitMission_카드없으면_nullSpot카드조회() {
-        Long missionId = 1L;
-        MissionSession missionSession = mock(MissionSession.class);
-        Spot spot = mock(Spot.class);
+        final Long missionId = 1L;
+        final MissionSession missionSession = mock(MissionSession.class);
+        final Spot spot = mock(Spot.class);
         spot.setId(10L);
-        Card card = mock(Card.class);
+        final Card card = mock(Card.class);
 
         when(missionSessionRepository.findById(missionId)).thenReturn(Optional.of(missionSession));
         when(missionSession.isStatusStarted()).thenReturn(true);
@@ -160,16 +165,16 @@ class MissionServiceTest {
         when(cardRepository.findAllBySpotId(spot.getId())).thenReturn(Collections.emptyList());
         when(cardRepository.findAllBySpotIsNull()).thenReturn(List.of(card));
 
-        MissionSubmitDto dto = missionService.submitMission(missionId, true);
+        final MissionSubmitDto dto = missionService.submitMission(missionId, true);
 
         assertThat(dto.successful()).isTrue();
     }
 
     @Test
     void submitMission_카드전혀없으면_예외() {
-        Long missionId = 1L;
-        MissionSession missionSession = mock(MissionSession.class);
-        Spot spot = mock(Spot.class);
+        final Long missionId = 1L;
+        final MissionSession missionSession = mock(MissionSession.class);
+        final Spot spot = mock(Spot.class);
         spot.setId(10L);
 
         when(missionSessionRepository.findById(missionId)).thenReturn(Optional.of(missionSession));
@@ -184,13 +189,13 @@ class MissionServiceTest {
 
     @Test
     void submitMission_실패케이스() {
-        Long missionId = 1L;
-        MissionSession missionSession = mock(MissionSession.class);
+        final Long missionId = 1L;
+        final MissionSession missionSession = mock(MissionSession.class);
 
         when(missionSessionRepository.findById(missionId)).thenReturn(Optional.of(missionSession));
         when(missionSession.isStatusStarted()).thenReturn(true);
 
-        MissionSubmitDto dto = missionService.submitMission(missionId, false);
+        final MissionSubmitDto dto = missionService.submitMission(missionId, false);
 
         assertThat(dto.successful()).isFalse();
     }
@@ -199,16 +204,16 @@ class MissionServiceTest {
 
     @Test
     void checkPossibleMissionSpot_미션없으면_true반환() {
-        Long teamId = 1L;
-        Long spotId = 2L;
-        Spot spot = mock(Spot.class);
-        Team team = mock(Team.class);
+        final Long teamId = 1L;
+        final Long spotId = 2L;
+        final Spot spot = mock(Spot.class);
+        final Team team = mock(Team.class);
 
         when(spotRepository.findById(spotId)).thenReturn(Optional.of(spot));
         when(teamRepository.findById(teamId)).thenReturn(Optional.of(team));
         when(missionSessionRepository.findByTeamAndSpot(team, spot)).thenReturn(Optional.empty());
 
-        MissionPossibleCheckDto dto = missionService.checkPossibleMissionSpot(teamId, spotId);
+        final MissionPossibleCheckDto dto = missionService.checkPossibleMissionSpot(teamId, spotId);
 
         assertThat(dto.isSuccess()).isTrue();
         assertThat(dto.getMessage()).isNull();
@@ -216,17 +221,17 @@ class MissionServiceTest {
 
     @Test
     void checkPossibleMissionSpot_미션있으면_false반환() {
-        Long teamId = 1L;
-        Long spotId = 2L;
-        Spot spot = mock(Spot.class);
-        Team team = mock(Team.class);
-        MissionSession session = mock(MissionSession.class);
+        final Long teamId = 1L;
+        final Long spotId = 2L;
+        final Spot spot = mock(Spot.class);
+        final Team team = mock(Team.class);
+        final MissionSession session = mock(MissionSession.class);
 
         when(spotRepository.findById(spotId)).thenReturn(Optional.of(spot));
         when(teamRepository.findById(teamId)).thenReturn(Optional.of(team));
         when(missionSessionRepository.findByTeamAndSpot(team, spot)).thenReturn(Optional.of(session));
 
-        MissionPossibleCheckDto dto = missionService.checkPossibleMissionSpot(teamId, spotId);
+        final MissionPossibleCheckDto dto = missionService.checkPossibleMissionSpot(teamId, spotId);
 
         assertThat(dto.isSuccess()).isFalse();
         assertThat(dto.getMessage()).isEqualTo(ResponseMessage.ALREADY_PROGRESSED_MISSION.getMessage());
